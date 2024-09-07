9bc74d420fc046130cab2f418663573073097748
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-07 14:45:41 +0545

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import img from "./assets/background.png";
+
 export const App = () => {
-  return <main></main>;
+  return (
+    <main
+      className="bg-brand min-h-screen w-screen"
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    ></main>
+  );
 };
